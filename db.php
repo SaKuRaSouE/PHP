6f5f3657332d59e695bb997bcdc2e8b0b01df891
SaKuRaSouE <6306021412016@fitm.kmutnb.ac.th>
2021-03-11 15:24:46 +0700

    update Db

--- db.php
+++ db.php
@@ -1,20 +1,50 @@
 <?php
+class Database
+{
+public $dbConn = Null;
+    public function connect()
+    {
     define("hostname","localhost");
     define("user","SaKuRaSou");
     define("password", "XDdBjqoDos5609zt");
     define("dbname","bookstore");
-?>
-
-
-
-<?php
-    $mysqli = new mysqli(hostname, user, password, dbname);
+    $this->dbConn = new mysqli(hostname, user, password, dbname);
+    $this->dbConn->query("SET NAMES UTF8");
     // Check connection
-    if ($mysqli -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-        exit();
+    if ($this->dbConn -> connect_errno) {
+        die("Database Connection Error, Error No.: " .
+                $this->dbConn->connect_errno . " | " .
+                $this->dbConn->connect->connect_error);
+               
     }else{
         echo "Connect success....";
     }
-    $sqltxt = ""
+}
+public function Show_Book(){
+        $SQL_Query = "SELECT `BookID`, `BookName`, `TypeID`, `StatusID`, `Publish`, `UnitPrice`, `UnitRent`, `DayAmount`, `BookDate` FROM `book` WHERE 1";
+        $result=$this->dbConn->query($SQL_Query);
+            echo "<table border='1'>";
+            $counter=0;
+            while ($row=$result->fetch_assoc()){
+                if($counter==0){
+                    echo "<tr><th colspan='10'><h1>Book Store</h1></th></tr>";
+                    echo "<tr>";
+                    foreach($row as $key=>$value){
+                        echo "<th>{$key}</th>";
+                    }
+                    echo "</tr>";
+                    $counter++;
+                }
+                echo "</tr>";
+                foreach($row as $key=>$value){
+                    echo "<th>{$value}</th>"; 
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+    }
+    public function disconnect(){
+        $this->dbConn->close();
+    }
+}
 ?>
