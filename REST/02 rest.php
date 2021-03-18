d2ead0d6df49340b49b619b49556b969d4690876
SaKuRaSouE <6306021412016@fitm.kmutnb.ac.th>
2021-03-18 15:00:55 +0700

    updata

---
+++ REST/02 rest.php
@@ -0,0 +1,27 @@
+<?php
+ include_once "01 db.php";
+ include_once "util.php";
+ $debug_mode = false;
+
+if ($_SERVER['REQUEST_METHOD']=='GET'){
+     debug_text("GET METHOD Process...",$debug_mode);
+    echo json_encode(show_data($debug_mode));
+}else if ($_SERVER['REQUEST_METHOD']=='POST'){
+     debug_text("POST METHPD May be implemnet soon...",$debug_mode);
+     $message = array ("Status"=>"POST Method");
+     echo json_encode($message);
+    }else{
+     debug_text("Error this site Unsupport This reqest",$debug_mode);
+     http_response_code(405);
+}
+
+ function show_data($debug_mode){
+     $mydb = new db ("root","","person",$debug_mode);
+     $data = $mydb->query("select * from person_data");
+    //  print_r($data);
+     $mydb->close();
+     return $data;
+ }
+ function add_data(){
+     
+ }
