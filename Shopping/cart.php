02a5b72e76450aff245bf3967d2880dde7dc13d0
SaKuRaSouE <6306021412016@fitm.kmutnb.ac.th>
2021-04-08 16:16:12 +0700

    update

--- Shopping/cart.php
+++ Shopping/cart.php
@@ -44,31 +44,36 @@
 					</thead>
 					<tbody>
                         <?php
+						$mysql = new mysqli("localhost", "root", "", "shopshock");
+						$mysql->set_charset("UTF8");
+						$sql = "SELECT product.Product_code, product.Product_Name, product.Cost , product.Stock_Quantity FROM `product`";
+						$result = $mysql->query($sql);
+						$rows = $result->fetch_all(MYSQLI_NUM);
                         show_detail();
-                        ?>
-                        <?php
-                            function show_detail(){
+                        $Total = 99;
+                       
+                        function show_detail($product_code='11', $product_name='T-Shirt', $price='12', $demand='1', $stock_quantity='5'){
                         ?>
 						<tr>
 							<td class="cart_product">
 								<a href=""><img src="images/home/product2.jpg" alt=""></a>
 							</td>
 							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
+								<h4><a href=""><?=$product_name ?></a></h4>
+								<p>Product ID : <?= $product_code ?></p>
 							</td>
 							<td class="cart_price">
-								<p>$59</p>
+								<p><?= $price ?></p>
 							</td>
 							<td class="cart_quantity">
 								<div class="cart_quantity_button">
 									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+									<input class="cart_quantity_input" type="number" name="quantity" min="1" max="<?=$stock_quantity ?>" value="<?= $demand ?>" autocomplete="off" size="2">
 									<a class="cart_quantity_down" href=""> - </a>
 								</div>
 							</td>
 							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
+								<p class="cart_total_price"><?= $price * $demand ?></p>
 							</td>
 							<td class="cart_delete">
 								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
@@ -76,7 +81,7 @@
                            
 						</tr>
                         <?php } ?>
-                            <tr><th colspan="6"><center><h1>Total 15 บาท</h1></center></th></tr>
+                            <tr><th colspan="6"><center><h1>ToTal <?= $Total ?></h1></center></th></tr>
 						
 					</tbody>
 				</table>
